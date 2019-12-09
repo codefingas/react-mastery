b8d0b5f0bbc635f333f4eed206528f31d9ef4e16
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2019-12-09 03:31:44 +0100

    conditionally rendered the ninja component

--- src/ninjas.js
+++ src/ninjas.js
@@ -1,21 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render() {
-        const {ninjas} = this.props;
-        const ninjasList = ninjas.map(ninja => {
-                    return (<div className="ninja" key={ninja.id}>
-                            <div>Name : {ninja.name}</div>
-                            <div>Age : {ninja.age}</div>
-                            <div>Belt : {ninja.belt}</div>
-                        </div>)
-                });
-    return (
-            <div className="ninja-list">
-                {ninjasList}
-            </div>
-        )
-    }
+const Ninjas = ({ninjas}) => { // destructured the props being recieved from app.js
+    const ninjasList = ninjas.map(ninja => {
+           return ninja.age > 20 ? ( 
+            <div className="ninja" key={ninja.id}>
+                <div>Name : {ninja.name}</div>
+                <div>Age : {ninja.age}</div>
+                <div>Belt : {ninja.belt}</div>
+            </div>) : null;
+           
+            });
+return (
+        <div className="ninja-list">
+            {ninjasList}
+        </div>
+    );
 };
 
 export default Ninjas;
